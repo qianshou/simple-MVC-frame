cf9bf2e0a674f36a061ac92b640795f7c7170c05
799631071@qq.com <799631071@qq.com>
2016-07-31 16:18:15 +0800

    增加了XSS过滤功能

--- libs/Request.php
+++ libs/Request.php
@@ -5,15 +5,17 @@
  * Date: 2016/6/1
  * Time: 15:53
  */
+require_once "Security.php";
 class Request{
     static public $post;
     static public $get;
     static public function create(){
+        $security = new CI_Security();
         foreach($_POST as $k => $v){
-            self::$post[$k] = $v;
+            self::$post[$k] = $security->xss_clean($v);
         }
         foreach($_GET as $k => $v){
-            self::$get[$k] = $v;
+            self::$get[$k] = $security->xss_clean($v);
         }
     }
 }
