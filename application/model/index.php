0a8d3904eea1b076dec640df5330e304ea7b963c
799631071@qq.com <799631071@qq.com>
2016-08-12 13:52:14 +0800

    创建相关目录

---
+++ application/model/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zhezhao
+ * Date: 2016/8/12
+ * Time: 8:59
+ */
+class Index
+{
+    function test(){
+        echo __CLASS__.":".__FUNCTION__;
+    }
+}
