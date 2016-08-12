65652a64e6ad11ab8e455a764bbb3cbf5383a033
799631071@qq.com <799631071@qq.com>
2016-08-12 20:25:45 +0800

    优化文件的布局

---
+++ application/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zhezhao
+ * Date: 2016/8/12
+ * Time: 20:24
+ */
+class Index extends Controller
+{
+    public function index(){
+        echo "hello framework.";
+    }
+}
---
+++ application/model/testModel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zhezhao
+ * Date: 2016/8/12
+ * Time: 20:25
+ */
+class testModel
+{
+    public function doTest(){
+        
+    }
+}
